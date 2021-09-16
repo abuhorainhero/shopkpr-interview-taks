d196c92dd67fe841f1cbcbf45dbccebbd1898b0e
abuhorainhero <abuhorain2016@gmail.com>
2021-09-16 23:19:12 +0600

    first commit

---
+++ src/components/groups/GroupTable.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
+import { MdModeEdit } from 'react-icons/md';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { api_url } from '../../App';
+import "../../scss/dashboard/Groups/GroupTable.scss";
+
+const GroupTable = () => {
+    const [groups, setGroups] = useState([])
+
+    useEffect(() => {
+        axios.get(`${api_url}/api/groups`)
+            .then(result => {
+                setGroups(result.data.data)
+            })
+            .catch(err => { console.log(err); });
+    }, []);
+    return (
+        <section id="group_table" className="mt-5">
+            <Table responsive striped bordered hover>
+                <thead className="table_header">
+                    <tr>
+                        <th>Group Id</th>
+                        <th>Group Name</th>
+                        <th>Created Date</th>
+                        <th>group Details</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        groups?.map(info => <tr key={info._id}>
+                            <td>{info._id}</td>
+                            <td>{info.groupName}</td>
+                            <td>{info.createdAt}</td>
+                            <td className="text-success">View Details</td>
+                            <td> <span className="text-success"><MdModeEdit /> Edit</span>  <span className="text-danger ms-3"><RiDeleteBin6Line /> Delete</span> </td>
+                        </tr>)
+                    }
+
+                </tbody>
+            </Table>
+        </section>
+    );
+};
+
+export default GroupTable;
---
+++ src/components/roles/RoleTable.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
+import { MdModeEdit } from 'react-icons/md';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { api_url } from '../../App';
+import "../../scss/dashboard/Role/RoleTable.scss";
+
+const RoleTable = () => {
+    const [roles, setRoles] = useState([])
+
+    useEffect(() => {
+        axios.get(`${api_url}/api/roles`)
+            .then(result => {
+                setRoles(result.data.data)
+            })
+            .catch(err => { console.log(err); });
+    }, []);
+
+    return (
+        <section id="role_table" className="mt-5">
+            <Table responsive striped bordered hover>
+                <thead className="table_header">
+                    <tr>
+                        <th>role Id</th>
+                        <th>role Name</th>
+                        <th>Created Date</th>
+                        <th>role Details</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        roles?.map(info => <tr key={info._id}>
+                            <td>{info._id}</td>
+                            <td>{info.roleName}</td>
+                            <td>{info.createdAt}</td>
+                            <td className="text-success">View Details</td>
+                            <td> <span className="text-success"><MdModeEdit /> Edit</span>  <span className="text-danger ms-3"><RiDeleteBin6Line /> Delete</span> </td>
+                        </tr>)
+                    }
+                   
+                </tbody>
+            </Table>
+        </section>
+    );
+};
+
+export default RoleTable;
